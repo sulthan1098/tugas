1e241b4687203c9e17abb00fc76b03a8cd6ba327
Ex1oz09 <riyo1907@gmail.com>
2018-11-22 16:03:21 +0700

    Belajar PHP Riyo

---
+++ rio/22-11-2018_Belajar_PHP.php
@@ -0,0 +1,59 @@
+<html>
+<head>
+
+<title>Belajar PHP</title>
+
+
+
+<?php 
+
+// $ex = "EX1OZ";
+// $_ex1 = "Hohoho";
+// $ex2 = "Cantengan";
+// $ex3 = "Poltar";
+
+
+// echo "<h1> $ex </h1>";
+// echo "Hallo Jelek ". $_ex1 . "<br>";
+// echo "Pulgoso ";
+// echo $ex2 . " Itu " . $ex3;
+
+// echo " <br> ";
+
+// $angka = 25000;
+// $angka2 = 25000;
+// $angka3 = $angka + 10 + $angka2;
+// $angka4 = 2.13;
+// echo $angka + $angka2;
+// echo " <br> ";
+// echo $angka3;
+
+// echo " <br> ";
+
+// echo round($angka4);
+// echo " <br> ";
+// echo "Nomor Urut Kamu adalah " . rand(1, 99);
+
+// $text= "Kamu jelek tau ga !";   
+// echo str_replace("jelek","ganteng", $text);
+
+$array = array('Merah', 'Kuning', 'Biru');
+print_r($array) ;
+echo ' <br> ';
+echo $array[2];
+
+
+$bambang = ['Baba', 'Biba', 'Sasa'];
+echo ' <br>';
+echo $bambang[2];
+
+
+
+?>
+
+
+czc
+
+
+</head>
+</html>
