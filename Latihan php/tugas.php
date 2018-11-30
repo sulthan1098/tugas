8e0d40d7d895c1349865e5dbcc8a958309063945
Ex1oz09 <riyo1907@gmail.com>
2018-11-30 11:18:34 +0700

    sate padank

---
+++ Latihan php/tugas.php
@@ -0,0 +1,25 @@
+<?php
+
+$A1=$_POST ['nilai1'];
+$A2=$_POST ['nilai2'];
+
+$Tambah= $A1 + $A2;
+$Kurang= $A1 - $A2;
+$Pilih=$_POST['pilih'];
+
+
+if(empty($A1 or $A2))
+else if ($Pilih=="Tentukan !")
+{
+    echo "Maaf Silakhan Pilih Arimatikanya";
+}
+else if($Pilih="Penjumlahan")
+{
+    echo "Hasil dari Penjumlahannya adalah . $Tambah";
+}
+else if($Pilih="Pengurangan")
+{
+    echo "Hasil dari Pengurangannya adalah . $Kurang";
+}
+?>
+
--- Riyo/asal.php
+++ Riyo/asal.php
@@ -48,7 +48,7 @@
       <td width="126">Angka pertama </td>
       <td width="8">:</td>
       <td width="196"><label>
-        <input type="text" name="satu" />
+        <input type="text" name="" />
       </label></td>
     </tr>
     <tr>
