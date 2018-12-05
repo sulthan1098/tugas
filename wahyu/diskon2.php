63ad024aff0a337adca236252106e05821bc3308
wahyu1709 <wahyufirman26@gmail.com>
2018-12-05 11:04:07 +0700

    gg

--- wahyu/diskon2.php
+++ wahyu/diskon2.php
@@ -2,19 +2,19 @@
 
 $bilangan1=$_POST['buah1'];
 $bilangan2=$_POST['buah2'];
-// $bilangan3=$_POST['buah3'];
+$bilangan3=$_POST['buah3'];
 $bayar=$_POST['bayar'];
 
-$bayar=$jumlah1-$diskon;
-$jumlah1=$bilangan1+$bilangan2;
-// $jumlah2=$bilangan1+$bilangan3;
+$bilangan1=134500;
+$bilangan2=11000;
+$bilangan3=15000;
 
-if($jumlah1 == "duren dan semangka"){
-    $jumlah1 = 135500;
-    $diskon=(($jumlah1*20)/100);
-    echo $bayar;
-}else{
-    $jumlah1 = 10000;
+switch($bayar){
+    case $bilangan1;
 }
+
+
+
+
 
 ?>
