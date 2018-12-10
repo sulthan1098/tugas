5efc107fda31240ecdf2dd1de2e47383c69b8271
wahyu1709 <wahyufirman26@gmail.com>
2018-12-10 16:34:13 +0700

    diskon3

--- wahyu/diskon.php
+++ wahyu/diskon.php
@@ -1,73 +1,104 @@
-<html>
-    <head>
-    <title>
-    Sistem Perhitungan Diskon Oleh Keri Rahayu
-    </title>
-    <style>
-    #hasil{width:500px;margin:0 auto;padding:5px;font-weight:bold;color:#f00;}
-    body{padding:0;margin:0;font-family:arial;font-size:12px;}
-    #main-wrapper{border:3px double #c3d9ff;padding:5px;width:550px;margin:20px auto 0;}
-    #main{text-align:left;padding:5px;background:#e8eefa;}
-    table{font-size:12px;width:450px;}
-    #efrih{border:1px solid #b3ccf7;background:#c3d9ff;cursor:pointer;padding:2px 24px;}
-    h1{font-size:18px;font-weight:bold;padding:10px 0;text-align:center;}
-    #efri{font-size:11px;padding:5px 0 0;color:#0000ff;text-align:center;}red{font-weight:bold;color:#f00;}
-    </style>
-    </head>
-    <body>
-    <div id="main-wrapper">
-    <div id="main">
-    <form method="POST" action="">
-    <h1>Sistem Menghitung Diskon</h1>
-    <table>
-    <tr>
-    <td>Masukan Nama Pelanggan</td>
-    <td><input type="text" name="bil1" /></td>
-    <tr>
-    <tr><br>
-    <td>Masukan Jumlah Uang yang dibayar</td>
-    <td><input type="text" name="bil2" /></td>
-    </tr>
-    <tr>
-    <td>
-    <label> <br> <br>
-    <input type="submit" name="Submit" value="Hitung" id="efrih">
-    </label>
-    <label>
-    <input type="reset" name="reset" value="Batal" id="efrih">
-    </label>
-    </td>
-    </tr>
-    </table>
-    </form>
-    <div id="hasil">
-    <?php error_reporting (E_ALL ^ E_NOTICE); ?>
-    <?php
-    //Oleh KERI RAHAYU http://www.esticahsetiaku.blogspot.com/
-    $bilangan1=$_POST['bil1'];
-    $bilangan2=$_POST['bil2'];
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Hasil</title>
+<style type="text/css">
+<!--
+.style1 {
+ font-size: xx-large;
+ font-weight: bold;
+ 
+}
+.style2 {font-size: xx-large; font-weight: bold; color: #990099; }
+-->
+</style>
+</head>
+ <body style='background: green';>
+ <h1 align="center" style="color:white;"><marquee>
+   <span class="style4">SELAMAT DATANG DI PROGRAM SEDERHANA KAMI</span>
 
-    if ($bilangan2 >= 10000000) {
-    $diskon=(($bilangan2*10)/100);
-    $bayar=($bilangan2-$diskon);
-    echo "Yang harus di bayar oleh $bilangan1 dengan Diskon 10% yaitu <blink><u>Rp. $bayar,-</u></blink>";}
+ 
+ </marquee>  </h1>
+ <HR SIZE = "15" NOSHADE>
+<body>
+<div align="center" class="style2">
+  <p class="style2">RENTAL MOBIL MAJU BERSAMA </p>
+</div>
+<form method="post" action="quis_form_awal.php">
 
-    elseif ($bilangan2 >0 & $bilangan2 < 10000000) {
-    $diskon=(($bilangan2*8)/100);
-    $bayar=($bilangan2-$diskon);
-    echo "Yang harus di bayar oleh $bilangan1 dengan Diskon 8% yaitu <blink><u>Rp. $bayar,-</u></blink>";}
+<table width="627" height="349" border="1" align="center" bgcolor="#00CC99">
+  <tr>
+    <td width="737" height="43"><div align="center"><span class="style1">HASIL</span></div></td>
+  </tr>
+  <tr>
+    <td><?php
+ $namamobil=$_POST['namamobil'];
+ 
+ if($namamobil =='AVP')
+ {
+ $harga=250000;
+ }
+ else if($namamobil =='Panther')
+ {
+ $harga=3000000;
+ }
+ else
+ {
+ $harga=350000;
+ }
+ 
+ 
+ 
+echo "KODE MOBIL     : ".$_POST['nama'];
+echo "<br />";
+echo "<br />";
+echo "JENIS MOBIL      : ".$_POST['namamobil'];
+echo "<br />";
+echo "<br />";
+echo "HARGA SEWA    : $harga ";
+echo "<br />";
+echo "<br />";
+echo "LAMA SEWA    : ".$_POST['nama2'];
+echo "<br />";
+echo "<br />";
+echo "JUMLAH MOBIL  : ".$_POST['nama3'];
+echo "<br />";
+echo "<br />";
 
-    elseif ($bilangan2 = 0) {}
 
 
-    ?>
-    </div>
-    </div>
-    <div id="keri">
-    Copyright © 2013 <a href="http://www.esticahsetiaku.blogspot.com/"
-    target="new">keri rahayu</a>. Hak Cipta dilindungi Undang-Undang
-    <?php echo date("  d-m-Y  H:i:s "); ?>
-    </div>
-    </div>
-    </body>
-    </html>
+$lamasewa = $_POST['nama2'];
+$jumlah = $_POST['nama3'];
+
+$totharga = $lamasewa * $jumlah * $harga;
+
+   if($totharga > 2000000){
+  
+   echo "Total Harga : Rp. $totharga <br>";
+      echo "Selamat Anda mendapatkan diskon 5% <br>";
+   $diskon = $totharga * (5/100);
+   $tharga = $totharga - $diskon;
+    echo "Total Diskon Anda : Rp. $diskon <br>";
+   echo "Total Yang Anda Bayar : Rp. $tharga <br>";
+   }
+  
+  else
+  
+   {
+      echo "Total Harga : Rp. $totharga <br>";
+      echo " Maaf Anda Tidak Mendapatkan Diskon <br>";
+   echo "Total Yang Anda Bayar : Rp. $totharga<br>";
+   }
+  
+?>
+    <p>&nbsp;</p>
+    <p align="center">
+       <input name="submit" type="submit" value="Kembali" />
+    </p></td>
+  </tr>
+</table>
+</body>
+
+
+</html>
--- wahyu/diskon2.php
+++ wahyu/diskon2.php
@@ -1,31 +1,54 @@
 <?php
 
-$buah1 = $_POST['buah1'];
-$buah2 = $_POST['buah2'];
-$buah3 = $_POST['buah3'];
-$bayar=$_POST['bayar'];
+// $buah1 = $_POST['buah1'];
+// $buah2 = $_POST['buah2'];
+// $buah3 = $_POST['buah3'];
+$harga = $_POST['bayar'];
+$diskon=0;
 
-if($buah1 == "Duren (Rp.124.500)"){
-    $buah1 = 124500;
+
+if(isset($_POST['masuk'])){
+    echo "kembalian:";
+
+if(isset($_POST['buah1'])){
+    $buah1=124500;
+}
+if(isset($_POST['buah2'])){
+    $buah2=11000;
+}
+if(isset($_POST['buah3'])){
+    $buah3=15000;
+}
+}
+
+
+if($buah1 and $buah2){
+    $hasil1=$buah1+$buah2;
+    $diskon=$hasil1*0.2;
+    $tharga1=$hasil1-$diskon;
+    $kembalian1=$harga-$tharga1;
+    echo $kembalian1;
+}
+else if($buah1 and $buah3){
+    $hasil2=$buah1+$buah3;
+    $diskon=$hasil2*0.1;
+    $tharga2=$hasil2-$diskon;
+    $kembalian2=$harga-$tharga2;
+    echo $kembalian2;
+}
+else if($buah2 and $buah3){
+    $hasil3=$buah2+$buah3;
+    $kembalian3=$harga-$hasil3;
+    echo $kembalian3;
+}
+else if($buah1 and $buah2 and $buah3){
+    $hasil4=$buah1+$buah2+$buah3;
+    $kembalian4=$harga-$hasil4;
+    echo $kembalian4;
 }
 else{
-    $buah1 = 124500;
+    echo "maaf";
 }
-
-if($buah2 == "Semangka (Rp.11.000)"){
-    $buah2 = 11000;
-}
-else{
-    $buah2 = 11000;
-}
-
-if($buah3 == "Melon (Rp.15.000)"){
-    $buah3 = 15000;
-}
-else {
-    $buah3 = 15000;
-}
-
 
 
 
