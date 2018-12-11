96a40595fc5a8d64198b19960dac6c716ea6cef0
Ex1oz09 <riyo1907@gmail.com>
2018-12-11 11:13:25 +0700

    bambnak

--- 30-11-2018_Tugas/Riyo/hitung.php
+++ 30-11-2018_Tugas/Riyo/hitung.php
@@ -4,7 +4,7 @@ $Barang1 = $_POST ['jajang'];
 $Barang2 = $_POST ['bambank'];
 $Nama1   = $_POST ['jajang'];
 $Nama2   = $_POST ['bambank'];
-$Hasil   = $Barang1 + $Barang2 ;
+
 
 if($Barang1 == "Buku (Rp 20.000)"){
     $Barang1 = 20000;
@@ -32,19 +32,11 @@ else{
     $Barang2 = 500;
 }
 
-if($Nama1 == "Buku (Rp 20.000)"){
-    
-}
-else if($Nama1 == "Pulpen (Rp 2.500)"){
-    
-}
-else if($Nama1 == "Penggaris (Rp 3.000)"){
-   
-}
-else{
-   
-}
 
-echo "Barang yang anda beli adalah : $Nama1";
+$Hasil   = $Barang1 + $Barang2 ;
+
+echo "Barang yang saya beli adalah $Nama1 & $Nama2";
+echo "<br>";
+echo "Harga yang harus saya bayar adalah $Hasil";
 
 ?>
--- wahyu/diskon2.php
+++ wahyu/diskon2.php
@@ -3,7 +3,13 @@
 $buah1 = $_POST['buah1'];
 $buah2 = $_POST['buah2'];
 $buah3 = $_POST['buah3'];
+$nama1 = $_POST['buah1'];
+$nama2 = $_POST['buah2'];
+$nama3 = $_POST['buah3'];
 $bayar=$_POST['bayar'];
+$diskon1 = 17100;
+$diskon2 = 13950;
+
 
 if($buah1 == "Duren (Rp.124.500)"){
     $buah1 = 124500;
@@ -26,7 +32,22 @@ else {
     $buah3 = 15000;
 }
 
+if($buah1 and $buah2 == "Duren (Rp.124.500)"  "Semangka (Rp.11.000)" ){
+    $buah1 + $buah2 - $diskon
+}
+else if($buah1 and $buah3 == "Duren (Rp.124.500)" and "Melon (Rp.15.000)" ){
+    $buah1 + $buah3 - $diskon2
+}
+else{
+    $buah1 + $buah2
+}
+
+
+if($nama1 == "Duren (Rp.124.500)"){
+    echo "Duren";
+}
 
 
 
+echo "Beli $nama1 Dan $nama2    "
 ?>
