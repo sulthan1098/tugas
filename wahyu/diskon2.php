24100f17e2f3d8406544ee49013f658044132dfa
Ex1oz09 <riyo1907@gmail.com>
2018-12-11 16:17:36 +0700

    Wawa

--- wahyu/diskon2.php
+++ wahyu/diskon2.php
@@ -15,12 +15,12 @@ if(isset($_POST['buah1'])){
     $buah1=124500;
 }
 if(isset($_POST['buah2'])){
-    echo $_POST['buah2']."<br>";
+    echo $_POST['buah2']. " & ";
     $buah2=11000;
 }
 if(isset($_POST['buah3'])){
-    echo $_POST['buah3']."<br>";
-    $buah3=15000;
+    echo  $_POST['buah3'] ."<br>";
+    $buah3=15000; 
 }
 }
 
@@ -40,22 +40,33 @@ function buah1()
 
 
 
+echo
 
-echo buah1();
 
 function buah2()
 {
  global $buah1,$buah3,$harga;
  $hasil2=$buah1+$buah3;
- $diskon=$hasil2*0.1;
- $tharga2=$hasil2-$diskon;
+ $diskon2=$hasil2*0.1;
+ $tharga2=$hasil2-$diskon2;
  $kembalian2=$harga-$tharga2;
  echo "Bayar  $harga <br>";
- echo "Mendapatkan Diskon Sebesar $diskon <br>";
+ echo "Mendapatkan Diskon Sebesar $diskon2 <br>";
  echo "Hasilnya adalah $kembalian2";
 }
 
-                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+function buah3(){
+    global $buah1,$buah2,$buah3,$harga;
+    $hasil3=$buah1+$buah2+$buah3;
+    $diskon3=$hasil3*0.05;
+    $tharga3=$hasil3-$diskon3;
+    $kembalian3=$harga-$tharga3;    
+    echo "Bayar  $harga <br>";~
+    echo "Mendapatkan Diskon Sebesar $diskon3 <br>";
+    echo "Hasilnya adalah $kembalian3";
 
+}                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+
+echo buah1();
 
 ?>
