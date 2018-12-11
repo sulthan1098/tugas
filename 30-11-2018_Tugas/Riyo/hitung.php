53c39563146a17a164893b7c2463de56d425493d
Ex1oz09 <riyo1907@gmail.com>
2018-12-11 12:06:24 +0700

    sabana

--- 30-11-2018_Tugas/Riyo/hitung.php
+++ 30-11-2018_Tugas/Riyo/hitung.php
@@ -32,32 +32,11 @@ else{
     $Barang2 = 500;
 }
 
-<<<<<<< HEAD
 
 $Hasil   = $Barang1 + $Barang2 ;
 
 echo "Barang yang saya beli adalah $Nama1 & $Nama2";
 echo "<br>";
 echo "Harga yang harus saya bayar adalah $Hasil";
-=======
-$Hasil   = $Barang1 + $Barang2 ;
-
-echo $Hasil;
-
-// if($Nama1 == "Buku (Rp 20.000)"){
-    
-// }
-// else if($Nama1 == "Pulpen (Rp 2.500)"){
-    
-// }
-// else if($Nama1 == "Penggaris (Rp 3.000)"){
-   
-// }
-// else{
-   
-// }
-
-// echo "Barang yang anda beli adalah : $Nama1";
->>>>>>> 31dd1af230f8948d7fa3dbe6f49d5397f8a15942
 
 ?>
--- wahyu/diskon2.php
+++ wahyu/diskon2.php
@@ -1,30 +1,17 @@
 <?php
 
-<<<<<<< HEAD
-$buah1 = $_POST['buah1'];
-$buah2 = $_POST['buah2'];
-$buah3 = $_POST['buah3'];
-$nama1 = $_POST['buah1'];
-$nama2 = $_POST['buah2'];
-$nama3 = $_POST['buah3'];
-$bayar=$_POST['bayar'];
-$diskon1 = 17100;
-$diskon2 = 13950;
-
-=======
 // $buah1 = $_POST['buah1'];
 // $buah2 = $_POST['buah2'];
 // $buah3 = $_POST['buah3'];
 $harga = $_POST['bayar'];
 $diskon=0;
->>>>>>> 31dd1af230f8948d7fa3dbe6f49d5397f8a15942
 
 
 if(isset($_POST['masuk'])){
     echo "Total Belanjaan: <br>";
 
 if(isset($_POST['buah1'])){
-    echo "beli ".$_POST['buah1']."&nbsp;dan ";
+    echo "beli ".$_POST['buah1']." & ";
     $buah1=124500;
 }
 if(isset($_POST['buah2'])){
@@ -36,6 +23,8 @@ if(isset($_POST['buah3'])){
     $buah3=15000;
 }
 }
+
+$nodikson = "Maaf ini tidak dapat diskon :)";
 
 function buah1()
 {
@@ -50,66 +39,23 @@ function buah1()
 }
 
 
+
+
 echo buah1();
 
 function buah2()
 {
-    global $buah1,$buah3,$harga;
-    $hasil2=$bu
+ global $buah1,$buah3,$harga;
+ $hasil2=$buah1+$buah3;
+ $diskon=$hasil2*0.1;
+ $tharga2=$hasil2-$diskon;
+ $kembalian2=$harga-$tharga2;
+ echo "Bayar  $harga <br>";
+ echo "Mendapatkan Diskon Sebesar $diskon <br>";
+ echo "Hasilnya adalah $kembalian2";
 }
 
-if($buah1 and $buah2 == "Duren (Rp.124.500)"  "Semangka (Rp.11.000)" ){
-    $buah1 + $buah2 - $diskon
-}
-else if($buah1 and $buah3 == "Duren (Rp.124.500)" and "Melon (Rp.15.000)" ){
-    $buah1 + $buah3 - $diskon2
-}
-else{
-    $buah1 + $buah2
-}
+                                                                                                                                                                                                                                                                                                                                                                                                                                                           
 
 
-if($nama1 == "Duren (Rp.124.500)"){
-    echo "Duren";
-}
-
-// if($buah1 and $buah2){
-//     $hasil1=$buah1+$buah2;
-//     $diskon=$hasil1*0.2;
-//     $tharga1=$hasil1-$diskon;
-//     $kembalian1=$harga-$tharga1;
-//     echo "Diskon ".$diskon."<br>";
-//     echo "Bayar ".$harga."<br>";
-//     echo "sisa bayar ".$kembalian1;
-// }
-// else if($buah1 and $buah3){
-//     $hasil2=$buah1+$buah3;
-//     $diskon=$hasil2*0.1;
-//     $tharga2=$hasil2-$diskon;
-//     $kembalian2=$harga-$tharga2;
-//     echo "Diskon ".$diskon."<br>";
-//     echo "Bayar ".$harga."<br>";
-//     echo $kembalian2;
-// }
-// else if($buah2 and $buah3){
-//     $hasil3=$buah2+$buah3;
-//     $kembalian3=$harga-$hasil3;
-//     echo "Diskon ".$diskon."<br>";
-//     echo "Bayar ".$harga."<br>";
-//     echo $kembalian3;
-// }
-// else if($buah1 and $buah2 and $buah3){
-//     $hasil4=$buah1+$buah2+$buah3;
-//     $kembalian4=$harga-$hasil4;
-//     echo "Diskon ".$diskon."<br>";
-//     echo "Bayar ".$harga."<br>";
-//     echo $kembalian4;
-// }
-// else{
-//     echo "maaf";
-// }
-
-
-
-echo "Beli $nama1 Dan $nama2    "
 ?>
