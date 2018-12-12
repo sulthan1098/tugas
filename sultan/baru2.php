08459598e2a37d3f4fc30ffb2c23e0e07edd8dbd
sulthan1098 <eprimerro@gmail.com>
2018-12-12 11:12:07 +0700

    gu

--- sultan/baru2.php
+++ sultan/baru2.php
@@ -1,13 +1,12 @@
 <?php
 $data =array('sulthan','wahyu','riyo');
 $data2=array('tkj','tkj','tkj');
-$data3=array('11','11','11');
+
 echo "<table border='1'>";
-echo "<tr><th>Nama</th><th>jurusan</th><th>kelas</th></tr>";
+echo "<tr><th>Nama</th><th>jurusan</th></tr>";
 
 foreach($data  as $hasil)
 foreach($data2 as $hasil2)
-foreach($data3 as $hasil3)
 {
     echo "<tr><td>$hasil</td><td>$hasil2</td></tr>"; 
 }
