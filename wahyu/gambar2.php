d02d83a826f9ac1fadaf6fd8ca464c852e592e55
wahyu1709 <wahyufirman26@gmail.com>
2018-12-20 16:40:34 +0700

    yrbf

--- wahyu/gambar2.php
+++ wahyu/gambar2.php
@@ -1,4 +1,15 @@
 <?php
+// $file = file_get_contents("http://www.belajar.local/konversi.php");
+// $json = json_decode($file, true);
+ 
+// foreach ($json as $key) {
+//     if (is_array($key)) {
+//         foreach ($key as $key => $value) {
+//             echo $key . ' : ' . $value . '<br />';
+//         }
+//     }
+// }
 
-echo $_POST['ba'];
+// echo $_POST['ba'];
+
 ?>
